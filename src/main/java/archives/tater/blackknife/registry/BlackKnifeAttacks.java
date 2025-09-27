6fbe7517a6d6784233058c2d099cc95b15dd29a9
ThePotatoArchivist <josiahfu@gmail.com>
2025-09-27 03:36:16 -0700

    tracking swords wip

--- src/main/java/archives/tater/blackknife/registry/BlackKnifeAttacks.java
+++ src/main/java/archives/tater/blackknife/registry/BlackKnifeAttacks.java
@@ -6,12 +6,17 @@ import archives.tater.blackknife.item.BlackKnifeItem.Attack;
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
 import net.fabricmc.fabric.api.event.registry.RegistryAttribute;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class BlackKnifeAttacks {
@@ -23,10 +28,25 @@ public class BlackKnifeAttacks {
         return Registry.register(ATTACKS, BlackKnife.id(path), attack);
     }
 
-    public static final Attack TEST1 = register("test1", new Attack() {
+    public static final Attack TRACKING_SWORDS = register("tracking_swords", new Attack() {
         @Override
         public ActionResult use(World world, PlayerEntity user, Hand hand) {
-            user.playSound(SoundEvents.ENTITY_RAVAGER_ROAR);
+            if (world.isClient()) return ActionResult.SUCCESS;
+            var start = user.getEyePos();
+            var end = start.add(user.getRotationVector().multiply(16));
+            var hit = ProjectileUtil.raycast(user, start, end, new Box(start, end), Entity::isLiving, 16 * 16);
+            if (hit == null) return ActionResult.FAIL;
+            var target = hit.getEntity();
+            var offset = new Vec3d(
+                    user.getRandom().nextBetween(-1, 1),
+                    user.getRandom().nextBetween(0, 1),
+                    user.getRandom().nextBetween(-1, 1)
+            ).multiply(target.getWidth() / 2 + 2);
+            var projectile = new ArrowEntity(world, user, Items.ARROW.getDefaultStack(), null);
+            projectile.setPosition(target.getEyePos().add(offset));
+            projectile.setVelocity(-offset.x, -offset.y, -offset.z, 3f, 1f);
+            world.spawnEntity(projectile);
+
             return ActionResult.SUCCESS;
         }
     });
